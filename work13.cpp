c48cf25cefe40def82798226b65b7e4ffd90fced
Sportik2020 <114169865+Sportik2020@users.noreply.github.com>
2022-10-27 18:13:57 +0300

    Add files via upload

---
+++ work13.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main() 
+{
+//7
+/*	int n, sum = 0;
+	double n2;
+	cin >> n;
+	for (int i = 1; i <= n; i++) {
+		cin >> n2;
+		cout << round(n2) << endl;
+		sum = sum +round(n2);
+	}
+	cout << sum << endl;*/
+//8
+/*	int n;
+	int sum = 0;
+	int n2;
+	cin >> n;
+	for (int i = 1; i <= n; i++) {
+		cin >> n2;
+		if (n2 % 2 == 0) {
+			cout << n2 << endl;
+			sum = sum + 1;
+		}
+	}
+	cout << sum << endl;*/
+//9
+/*	int n;
+	int sum = 0;
+	int n2;
+	cin >> n;
+	for (int i = 1; i <= n; i++) {
+		cin >> n2;
+		if (n2 % 2 != 0) {
+			cout << n2 << endl;
+			sum = sum + 1;
+		}
+	}
+	cout << sum << endl;*/
+//10
+/*	int n,n2;
+	int c = 0;
+	cin >> n;
+	int i = 1;
+	while (i <= n){
+		cin >> n2;
+		if (n2 > 0) {
+			c += 1;
+		}
+		i++;
+	}
+	if (c > 0) {
+		cout << "true" << endl;
+	}
+	else {
+		cout << "false" << endl;
+	}*/
+//11
+/*	int k, n;
+	int a, b = 0;
+	cin >> n >> k;
+	int i = 1;
+	while (i <= n) {
+		cin >> a;
+		if ( a < k ) {
+			b += 1;
+		}
+		i++;
+	}
+	if (b > 0) {
+		cout << "true" << endl;
+	}
+	else {
+		cout << "false" << endl;
+	}*/
+	return 0;
+}
